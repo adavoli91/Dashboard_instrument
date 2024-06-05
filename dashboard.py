2a886b32d4cda2092bd3ae9d6e9ea0e6c09c354d
adavoli91 <alessandro.davoli91@gmail.com>
2024-06-05 22:20:24 +0200

    Fix backtest

--- dashboard.py
+++ dashboard.py
@@ -757,7 +757,7 @@ class Dashboard:
             else:
                 st.write(dashboard.col_x)
                 for breakdown in df[dashboard.col_color].unique():
-                    if dashboard.col_x != 'day of month':
+                    if dashboard.col_x != 'Day of month':
                         figure.add_trace(go.Scatter(x = df.loc[df[dashboard.col_color] == breakdown, dashboard.col_x],
                                                     y = df.loc[df[dashboard.col_color] == breakdown, 'Metric'],
                                                     name = f'{breakdown}', mode = 'lines'))
@@ -770,7 +770,7 @@ class Dashboard:
                 figure.add_trace(go.Bar(x = df[dashboard.col_x], y = df['Metric'], width = 0.5, offset = -0.5))
             else:
                 for breakdown in df[dashboard.col_color].unique():
-                    if dashboard.col_x != 'day of month':
+                    if dashboard.col_x != 'Day of month':
                         figure.add_trace(go.Bar(x = df.loc[df[dashboard.col_color] == breakdown, dashboard.col_x],
                                                     y = df.loc[df[dashboard.col_color] == breakdown, 'Metric'],
                                                     name = f'{breakdown}', width = 0.5, offset = -0.5))
