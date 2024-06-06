1ef4b3b6bf0fcfd9c6f9be8282798e204454e01f
adavoli91 <alessandro.davoli91@gmail.com>
2024-06-06 10:00:18 +0200

    Fix session start

--- dashboard.py
+++ dashboard.py
@@ -833,7 +833,7 @@ class Dashboard:
         if start_sess == '17:00:00':
             st.write(df)
             figure.add_vrect(x0 = df.loc[np.where(df['Time'] >= '17:00:00')[0].min(), 'Time'],
-                             x1 = df.loc[np.where(df['Time'] <= '23:59:00')[0].max(), 'Time'], fillcolor = 'yellow', opacity = 0.15, line_width = 0)
+                             x1 = df['Time'].max(), fillcolor = 'yellow', opacity = 0.15, line_width = 0)
             # figure.add_annotation(x = '20:30:00', y = df['Metric'].min()*1.1, text = 'Asia', font = {'size': 18, 'color': 'white'}, yanchor = 'top')
             # figure.add_vrect(x0 = '17:00:00', x1 = '23:59:00', fillcolor = 'yellow', opacity = 0.15, line_width = 0)
             figure.add_annotation(x = '21:00:00', y = df['Metric'].min()*1.1, text = 'Asia', font = {'size': 18, 'color': 'white'}, yanchor = 'top')
