7c9383d009be3bc73c57b0508f85f7db348ec93f
adavoli91 <alessandro.davoli91@gmail.com>
2024-06-13 18:43:54 +0200

    Fix weekday

--- dashboard.py
+++ dashboard.py
@@ -532,6 +532,7 @@ class Dashboard:
         Returns: None.
         '''
         df = self.df.copy()
+        # df.to_pickle('./aa.pickle.gz')
         self.col_x = 'date'
         self.format_x = '%Y-%m-%d %H:%M:%S'
         self.col_color = None
@@ -680,6 +681,7 @@ class Dashboard:
                 df = df.drop('time', axis = 1)
                 self.col_x = 'day of month'
             #
+            # df.to_pickle('./aa2.pickle.gz')
             self.df = df
 
     def _adjust_timeframe(self):
